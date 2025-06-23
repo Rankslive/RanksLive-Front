d7d254c006f97e218142dde71e86e1c4a779662d
lonewolfyx <877085893@qq.com>
2025-06-23 15:19:38 +0800

    fix: home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,34 +3,31 @@
 		<div class="z-20">
 			<Menubar />
 		</div>
-		<div class="relative overflow-x-auto w-full h-full p-4 px-28 z-20">
-			<swiper
-				:keyboard="{enabled: true}"
-				:modules="modules"
-				:mousewheel="true"
-				:navigation="false"
-				:pagination="{clickable: true}"
-				:slidesPerView="1"
-				:spaceBetween="30"
-			>
-				<swiper-slide>
-					<div class="grid grid-cols-5 gap-4">
-						<a-card
-							v-for="item in Array.from({length:15}, (_, i) => i+1)"
-							:key="'2222'+item"
-							title="Arco Card"
-							:body-style="{height:'250px'}"
-						>
-							<ul class="p-0">
-								<li>
-									<a href="@#" class="text-white">asdas</a>
-								</li>
-							</ul>
-						</a-card>
+		<div class="relative overflow-x-auto w-full h-full py-4 px-6 z-20">
+			<div class="grid grid-cols-5 gap-5">
+				<a-card
+					v-for="item in Array.from({length:30}, (_, i) => i+1)"
+					:key="'2222'+item"
+					:body-style="{paddingTop:0}"
+					class="mb-5"
+					title="Arco Card"
+				>
+					<div class="rank-content relative h-96 overflow-scroll gap-4">
+						<ul class="p-0 space-y-3">
+							<li
+								v-for="items in Array.from({length:30}, (_, i) => i+1)"
+								:key="'2222'+items"
+							>
+								<a class="flex justify-between items-center text-slate-200" href="@#">
+									<span>1.</span>
+									<span class="mr-auto line-clamp-1">95岁离休干部被认定为恶势力主犯95岁离休干部被认定为恶势力主犯</span>
+									<span>1457324</span>
+								</a>
+							</li>
+						</ul>
 					</div>
-				</swiper-slide>
-			</swiper>
-
+				</a-card>
+			</div>
 		</div>
 		<div class="z-20">
 			<MacDock />
@@ -41,20 +38,16 @@
 <script lang="ts" setup>
 import Menubar from '@/components/Menubar/Menubar.vue'
 import MacDock from '@/components/Docker/Docker.vue'
-import { Swiper, SwiperSlide } from 'swiper/vue'
-import { Keyboard, Mousewheel, Navigation, Pagination, Virtual } from 'swiper/modules'
+import { useLand } from '@/hooks/useLand.ts'
 
-import 'swiper/scss'
-import 'swiper/scss/pagination'
-import 'swiper/scss/navigation'
-import 'swiper/scss/virtual'
-
-const modules = [Keyboard, Pagination, Navigation, Mousewheel, Virtual]
+useLand({
+	message:'欢迎来到热门榜单数据平台'
+})
 </script>
 
 <style lang="scss" scoped>
 .rank-container {
-	@apply relative w-full h-full grid grid-rows-[30px_1fr_64px];
+	@apply relative w-full h-full grid grid-rows-[30px_1fr_80px];
 	background-image: url('@/assets/images/bg1.jpg');
 	background-repeat: no-repeat;
 	background-size: cover;
@@ -71,4 +64,30 @@ const modules = [Keyboard, Pagination, Navigation, Mousewheel, Virtual]
 	}
 }
 
+.rank-content {
+
+	&::-webkit-scrollbar {
+		width: 4px;
+		height: 4px;
+	}
+
+	&::-webkit-scrollbar-track {
+		border-radius: 0;
+		background-color: transparent;
+	}
+
+	&::-webkit-scrollbar-thumb {
+		border-radius: 0;
+		background-color: transparent;
+	}
+
+	&:hover::-webkit-scrollbar-thumb {
+		background-color: #efefef;
+	}
+
+	&::-webkit-scrollbar-corner {
+		background-color: transparent;
+	}
+
+}
 </style>
